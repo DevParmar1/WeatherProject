ce968137adb72c0b79f2b9b88b74b58ca4181064
Dev Parmar <61654319+DevParmar1@users.noreply.github.com>
2020-10-04 02:38:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ res.sendFile(__dirname+"/index.html");
 app.post("/",function(req,res){
 
 
-  const query=req.body.cityName;
-  const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid=8c5ba2d149bb949ab7554c0fcabef0ec&units=metric";
+  const queries=req.body.cityName;
+  const url="https://api.openweathermap.org/data/2.5/weather?q="+queries+"&appid=8c5ba2d149bb949ab7554c0fcabef0ec&units=metric";
   https.get(url,function(response){
     console.log(response.statusCode);
 
@@ -28,7 +28,7 @@ app.post("/",function(req,res){
     const desc=weatherData.weather[0].description;
     const icon=weatherData.weather[0].icon;
     const imgUrl="http://openweathermap.org/img/wn/"+icon+"@2x.png";
-    res.write("<h1>The current Temperature in "+query+" is " +Temp+"</h1>");
+    res.write("<h1>The current Temperature in "+queries+" is " +Temp+"</h1>");
     res.write("<p>Today's Weather description is "+desc+"</p>");
     res.write("<img src="+imgUrl+ ">");
     res.send();
